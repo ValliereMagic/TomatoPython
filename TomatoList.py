ae340509b6169d6b1339974993b95c67ba362f19
FRadford <francis.radford@outlook.com>
2017-05-15 13:31:59 -0400

    Added Database System (#3)
    * Added sqlite3 database for saving serialized data
    
    * Optimized database interaction
    
    * Revert "Created database system"
    
    * Added license

--- TomatoList.py
+++ TomatoList.py
@@ -11,18 +11,18 @@ class TomatoList(list):
         print("Enter a new tomato to add to the tomato class \n")
 
         print("Enter the name of the tomato: \n")
-        tomato_name = input()
+        tomato_name = input().lower()
 
         print("Enter the colour of the tomato: \n")
-        tomato_colour = input()
+        tomato_colour = input().lower()
 
         print("Enter the nationality of the tomato: \n")
-        tomato_nationality = input()
+        tomato_nationality = input().lower()
 
         return Tomato.Tomato(tomato_name, tomato_colour, tomato_nationality)
 
     def delete_tomato(self, tomato_name):
-        tomato_to_delete = self.get_tomato(tomato_name)
+        tomato_to_delete = self.get_tomato(tomato_name.lower())
 
         if tomato_to_delete is not None:
             self.remove(tomato_to_delete)
@@ -38,11 +38,13 @@ class TomatoList(list):
                   "Tomato Nationality: " + tomato.get_tomato_nationality() + "\n")
 
     def describe_tomato(self, tomato_name):
-        tomato = self.get_tomato(tomato_name)
+        tomato = self.get_tomato(tomato_name.lower())
 
         if tomato is not None:
             print("Tomato Colour: " + tomato.get_colour() + "\n" +
                   "Tomato Nationality: " + tomato.get_tomato_nationality())
+        else:
+            print("Tomato:", tomato_name, "not found")
 
     def get_tomato(self, tomato_name):
 
