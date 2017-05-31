1ef82b855f3779c651ee87f517f4a538e57d6d53
FRadford <francis.radford@outlook.com>
2017-05-31 11:35:19 -0400

    Finished optimizations

--- Apple.py
+++
@@ -1,6 +0,0 @@
-from Fruit import Colourable
-
-
-class Apple(Colourable):
-    def __init__(self, name, colour, nationality):
-        super(Apple, self).__init__(name, colour, nationality)
--- Tomato.py
+++
@@ -1,6 +0,0 @@
-from Fruit import Colourable
-
-
-class Tomato(Colourable):
-    def __init__(self, tomato_name, tomato_colour, tomato_nationality):
-        super(Tomato, self).__init__(tomato_name, tomato_colour, tomato_nationality)
